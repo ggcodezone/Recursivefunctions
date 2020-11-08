6d99a17fe5ff37af8d3bc3413d7f63248b754759
Dank <ggkalasky@gmail.com>
2020-11-08 17:02:45 -0500

    Two apps (Random Number Generator and Prime Number Finder) which use recursive functions

---
+++ recursion2/recursion2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//Name:Prime Number Finder
+//Author: Gwen Kalasky
+//Synopsis: This program will tell you whether or not the input is a prime number
+//Date: Nov. 5th 2020
+namespace recursion2
+{
+    class Primefind
+    {
+        // returns true if n is prime, and if it isnt, returns false. 
+        static bool isPrime(int n, int i)
+        {
+
+            // Base cases 
+            if (n <= 2)
+                return (n == 2) ? true : false;
+            if (n % i == 0)
+                return false;
+            if (i * i > n)
+                return true;
+
+            // Check for next divisor 
+            return isPrime(n, i + 1);
+        }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                int n = 25; //put the number you want to check here
+
+                if (isPrime(n, 2))
+                    Console.Write("Yes"); //the output seen by user 
+                    Console.Write("No");
+            }
+
+        }
+    }
+    }
+}
---
+++ recurssion/recurssion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//Name:Random Number Generator
+//Author:Gwen Kalasky
+//Synopsis: Will generate a random number based on parameters set by user
+//Date: Nov. 4th 2020
+namespace recurssion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string allowedCharacters = "1234567890";
+           Random rnd = new Random();
+             string randomString(int length)
+            {
+                if (length == 0)
+                    return "";
+                return allowedCharacters[rnd.Next(0, allowedCharacters.Length)]
+                       + randomString(length - 1);   // the recursive call
+            }
+
+            // you can use this to display a random number (the max and min length of which is determined
+            //in the brackets after "rnd.Next")
+            Console.WriteLine(randomString(rnd.Next(1, 4)));
+
+        }
+    }
+}
